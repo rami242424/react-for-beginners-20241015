973e54716aa1517930b6e94054076124ed85275c
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-15 23:46:13 +0900

    #7.5 React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { to } from "./../../node_modules/prettier/plugins/typescript";
+
 function Movie({ coverImg, title, year, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        {title} ({year})
+        <Link to="/movie">
+          {title} ({year})
+        </Link>
       </h2>
       <p>{summary}</p>
       <ul>
