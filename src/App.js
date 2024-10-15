859f6debff9aa8e80e1b003fce6e899f133dc844
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-15 17:16:30 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-function Hello() {
-  // 방법1
-  function byeFn(){
-    console.log("Bye :(");
-  }
-  function HiFn(){
-    console.log("Hi :)");
-    return byeFn;
-  }
-  useEffect(HiFn, []);
-
-  // 방법2
-  // useEffect(() => {
-  //   console.log("Hi :)");
-  //   return () => console.log("Bye :(");
-  // }, []);
-  
-  // 방법3
-  // useEffect(function(){
-  //   console.log("Hi :)");
-  //   return function() {
-  //     console.log("Bye :(");
-  //   }
-  // }, []);
-  
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return; // toDo가 비어있다면 이 함수 작동 x (killed)
+    }
+    setTodo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      { showing? <Hello/> : null}
-      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
-
-  )
+  );
 }
 
 export default App;
