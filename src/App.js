34d6171dfa96ae5a6f89cb40c63e42268b0bc7ea
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-17 13:20:15 +0900

    6.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Button from "./Button";
+import { useState } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  console.log("render");
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
