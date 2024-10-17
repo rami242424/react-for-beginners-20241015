fe8b9d5b681e418f96f63153bc1137eecd3ec29e
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-17 13:38:28 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => {
     setValue((prev) => prev + 1);
   };
-  console.log("render");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("call the api");
+  }, []); // 처음 한번만 실행한다.
+
+  useEffect(() => {
+    if (keyword !== "") {
+      // 빈값이 아닐때만 keyword 검색하자
+      console.log("서치중...", keyword);
+    }
+  }, [keyword]); // keyword가 변할때만 실행한다.
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here...."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
